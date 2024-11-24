2f7589e46474903868665c1eeedff4d1b8c1538a
dma-neves <david.m.neves@live.com>
2024-11-24 18:20:50 +0000

    with driver

--- examples/elevator_with_driver.cpp
+++ examples/elevator_with_driver.cpp
@@ -57,6 +57,64 @@ private:
     close_door_timer& close_door_timer_;
 };
 
+class elevator_driver {
+
+public:
+
+    elevator_driver(event<ElevatorState>& reached_floor) : reached_floor_(reached_floor) {
+
+    }
+
+    void update(int increment = 1) {
+
+        if(moving_up) {
+            current_floor += increment;
+
+            if(current_floor >= destination_floor) {
+                current_floor = destination_floor;
+                reached_floor_.trigger();
+                moving_up = false;
+                moving_down = false;
+            }
+        }
+        else if(moving_down) {
+            current_floor -= increment;
+
+            if(current_floor <= destination_floor) {
+                current_floor = destination_floor;
+                reached_floor_.trigger();
+                moving_up = false;
+                moving_down = false;
+            }
+        }
+     }
+
+    void move_up() { 
+
+        moving_up = true;
+        moving_down = false;
+    }
+
+    void move_down() {
+
+        moving_up = false;
+        moving_down = true;
+    }
+    
+    void open_door() { /* control elevator door motors to open */ }
+    void close_door() { /* control elevator door motors to close */ }
+
+    void set_destination_floor(int floor) { destination_floor = floor; }
+private:
+    int destination_floor = 0;
+    int current_floor = 0;
+
+    bool moving_up = false;
+    bool moving_down = false;
+
+    event<ElevatorState>& reached_floor_;
+};
+
 int main() {
 
     close_door_timer close_door_timer_;
@@ -78,6 +136,8 @@ int main() {
     event_group<ElevatorState> go_down_({&select_floor_below_elevator_floor_, &press_up_below_elevator_floor_, &press_down_below_elevator_floor_});
     event_group<ElevatorState> go_up_({&select_floor_above_elevator_floor_, &press_up_above_elevator_floor_, &press_down_above_elevator_floor_});
 
+    elevator_driver ed(reached_selected_floor_);
+
     tsm<ElevatorState> elevator_sm(ElevatorState::DoorClosed, 
         {
             {{ElevatorState::DoorOpen, ElevatorState::DoorClosed}, close_door_timer_},
@@ -88,7 +148,14 @@ int main() {
             {{ElevatorState::MovingUp, ElevatorState::DoorOpen}, reached_selected_floor_},
             {{ElevatorState::DoorClosed, ElevatorState::MovingDown}, go_down_},
             {{ElevatorState::MovingDown, ElevatorState::DoorOpen}, reached_selected_floor_},
-        }
+        },
+        {
+            {ElevatorState::DoorClosed, [&ed](){ ed.close_door();   } },
+            {ElevatorState::DoorOpen,   [&ed](){ ed.open_door();    } },
+            {ElevatorState::MovingUp,   [&ed](){ ed.move_up();      } },
+            {ElevatorState::MovingDown, [&ed](){ ed.move_down();    } }
+        },
+        {}
     );
 
     print_elevator_state(elevator_sm.get_state()); // DoorClosed
@@ -109,18 +176,20 @@ int main() {
     close_door_timer_.update(3);
     print_elevator_state(elevator_sm.get_state()); // DoorClosed
 
+    ed.set_destination_floor(10);
     select_floor_above_elevator_floor_.trigger();
     print_elevator_state(elevator_sm.get_state()); // MovingUp
 
-    reached_selected_floor_.trigger();
+    ed.update(10);
     print_elevator_state(elevator_sm.get_state()); // DoorOpen
 
     close_door_timer_.update(3);
     print_elevator_state(elevator_sm.get_state()); // DoorClosed
 
+    ed.set_destination_floor(2);
     press_down_below_elevator_floor_.trigger();
     print_elevator_state(elevator_sm.get_state()); // MovingDown
 
-    reached_selected_floor_.trigger();
+    ed.update(8);
     print_elevator_state(elevator_sm.get_state()); // DoorOpen
 }
